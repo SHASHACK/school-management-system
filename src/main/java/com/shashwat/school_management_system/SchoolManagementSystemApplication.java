a400fd32c901dc77ef72ae1f3df668fa0fd010d1
SHASHWAT SRIVASTAVA <shash.knuth.srivastava@gmail.com>
2018-06-23 16:33:19 +0530

    Initial Commit

---
+++ src/main/java/com/shashwat/school_management_system/SchoolManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.shashwat.school_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSystemApplication.class, args);
+	}
+}
